e33f5039981de887dc00b4e25ba3a279cc86379c
P. Tsurkevych <p.tsurkevych@gmail.com>
2021-07-13 00:01:42 +0300

    .

--- sw.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-self.addEventListener('install', (event) => {});
-self.addEventListener('activate', (event) => {});
-self.addEventListener('fetch', (event) => {});
-importScripts("sw-toolbox.js");
-toolbox.precache(['index.php','/css/main.bundle.css']);
-toolbox.router.get("/images/*", toolbox.cacheFirst); 
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
